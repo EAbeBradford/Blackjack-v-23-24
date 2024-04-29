10f660bc5b204cd111ff098aa7a738e4c71e5e91
Mx. Bradford <emma_bradford@milton.com>
2024-04-29 13:09:12 -0400

    set up classes!

---
+++ src/Card.java
@@ -0,0 +1,6 @@
+public class Card {
+    // soiut  ex heart, club, spade, diamond
+    // value  ex worth 1-11 points in game
+    // type  ex ace, 1, 2, ... Jack, Queen, King
+
+}
---
+++ src/Casino.java
@@ -0,0 +1,10 @@
+public class Casino {
+    public static void main(String[] args) {
+        Casino c  = new Casino();
+        //Casino();
+    }
+
+    public  Casino(){
+        System.out.println("Welcome to Mx. Bradford's Casino!");
+    }
+}
